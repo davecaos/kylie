e8d59b031d3635845b357f44bd3b3010533b6b54
David Cao <david.c.h.cao@gmail.com>
2016-10-15 22:57:08 -0300

    Add Elixir binding with rebar3_elixir plugin (#2)
    * Add Elixir binding with rebar3_elixir plugin
    
    * Add Elixir binding with rebar3_elixir plugin

---
+++ lib/Squad.ex
@@ -0,0 +1,39 @@
+# File: Squad.ex
+# This file was generated from squad.beam
+# Using rebar3_elixir (https://github.com/botsunit/rebar3_elixir)
+# MODIFY IT AT YOUR OWN RISK AND ONLY IF YOU KNOW WHAT YOU ARE DOING!
+defmodule Squad do
+  def unquote(:"new")(arg1, arg2, arg3) do
+    :erlang.apply(:"squad", :"new", [arg1, arg2, arg3])
+  end
+  def unquote(:"new")(arg1, arg2, arg3, arg4) do
+    :erlang.apply(:"squad", :"new", [arg1, arg2, arg3, arg4])
+  end
+  def unquote(:"subject")(arg1) do
+    :erlang.apply(:"squad", :"subject", [arg1])
+  end
+  def unquote(:"predicate")(arg1) do
+    :erlang.apply(:"squad", :"predicate", [arg1])
+  end
+  def unquote(:"object")(arg1) do
+    :erlang.apply(:"squad", :"object", [arg1])
+  end
+  def unquote(:"label")(arg1) do
+    :erlang.apply(:"squad", :"label", [arg1])
+  end
+  def unquote(:"subject")(arg1, arg2) do
+    :erlang.apply(:"squad", :"subject", [arg1, arg2])
+  end
+  def unquote(:"predicate")(arg1, arg2) do
+    :erlang.apply(:"squad", :"predicate", [arg1, arg2])
+  end
+  def unquote(:"object")(arg1, arg2) do
+    :erlang.apply(:"squad", :"object", [arg1, arg2])
+  end
+  def unquote(:"label")(arg1, arg2) do
+    :erlang.apply(:"squad", :"label", [arg1, arg2])
+  end
+  def unquote(:"nquads_file")(arg1) do
+    :erlang.apply(:"squad", :"nquads_file", [arg1])
+  end
+end
